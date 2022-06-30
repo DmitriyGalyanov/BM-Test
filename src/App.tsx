58f3272eaadf86f31b439f6ecba27345fb024961
dmitriygalyanov <dimagalyanov@gmail.com>
2022-06-30 09:47:09 +0300

    wrap app with required providers [application is not able to work at this commit: screens and API are not defined yet]
    add RootNavigator wrapped with
    add NavigationContainer, [react-redux]Provider and SafeAreaView to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { store } from 'application/state/redux/store';
+import { RootNavigator } from 'application/navigation/navigators';
+import { screenWrap } from 'shared/consts/styles';
 
 const App = () => {
-  return <SafeAreaView />;
+  return (
+    <NavigationContainer>
+      <Provider store={store}>
+        <SafeAreaView style={screenWrap}>
+          <RootNavigator />
+        </SafeAreaView>
+      </Provider>
+    </NavigationContainer>
+  );
 };
 
 export default App;
