6645c3ce211e7708d0636c93d754510cc0c20789
dmitriygalyanov <dimagalyanov@gmail.com>
2022-06-30 09:40:20 +0300

    add absolute paths
    add babel-pluhig-module-resolver, config babel to use it
    
    add 'baseUrl' and 'paths' fields to tsconfig

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+          '.svg',
+        ],
+        root: ['./src'],
+      },
+    ],
+  ],
   presets: ['module:metro-react-native-babel-preset'],
 };
