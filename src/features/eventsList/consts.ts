00d94b581fc9734882024d9898fa752b18256f70
dmitriygalyanov <dimagalyanov@gmail.com>
2022-06-30 09:48:06 +0300

    events list feature in progress: add consts
    [application is not able to work at this commit: feature is not finished yet]

---
+++ src/features/eventsList/consts.ts
@@ -0,0 +1,5 @@
+export const INIT_IS_TIMER_ON = true;
+export const EVENTS_PER_PAGE = 25;
+export const DELAY_PER_FETCH = 60000;
+export const DELAY_BEFORE_USER_CAN_REFETCH = 15000;
+export const ON_SCROLL_END_DRAG_HANDLER_DELAY = 2000;
